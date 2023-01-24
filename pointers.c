16b78d16c1bb0a656769f08aa69877c5fcb063fe
Barani05072002 <123085284+Barani05072002@users.noreply.github.com>
2023-01-24 21:11:18 +0530

    first commit for my practice

---
+++ pointers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void change(int x,int y)    //formal perameters
+{
+    x=20;
+    y=10;
+}
+void inter(int *x,int *y)   //here passing the addresses
+{
+    *y=10;
+    *x=20;
+}
+int main()
+{
+    int i=50;
+    int *p;
+    p = &i; //pointer p holds the address of i
+    printf("value of i %d\n",i);
+    printf("address of i %d\n",&i);
+    printf("p pointer point value is %d\n",*p);
+    printf("p pointer holds the address %d\n",p);
+    printf("address of p %d\n",&p);
+    printf("%p\n",&i);
+    printf("%p\n",&p);
+    
+    //call by value
+    int x=10,y=20;          //actual perameters
+    printf("call by valye \nbefore change x=%d  y=%d\n",x,y);
+    change(x,y);
+    printf("after change  x=%d  y=%d\n",x,y);
+    
+    //call by reference
+    printf("call by reference \nbefore change x=%d y=%d\n",x,y);
+    inter(&x,&y);
+    printf("after change x=%d y=%d\n",x,y);
+    return 0;
+}
